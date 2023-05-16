5a88b0d9313848894bfe120eb94ff8d32062c302
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-05-16 13:34:00 +0800

    refactor: use latest  enabled_log block for azurerm_monitor_diagnostic_setting

--- src/modules/diagnostic_settings/main.tf
+++ src/modules/diagnostic_settings/main.tf
@@ -1,45 +1,22 @@
 
-
-# https://www.mediaglasses.blog/2021/08/30/some-terraform-azure-notes/
-
-# https://github.com/claranet/terraform-azurerm-diagnostic-settings/blob/master/locals.tf
-
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/monitor_diagnostic_categories
-
-# data "vsphere_datastore" "datastore" {
-#   for_each = var.CwVMs
-
-#   name          = each.value.Datastore
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
-
-# resource "vsphere_virtual_machine" "dev-vm" {
-#   for_each = var.CwVMs
-#   name  = "crosswork-${each.key}"
-
-#   datastore_id     = data.vsphere_datastore.datastore[each.key].id
-# }
-
-
 # intentionally ignore metric, only targeting logs
 resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
-    count = (local.law_id == "") ? 0 : length(var.resource_ids)
+    #count = (local.law_id == "") ? 0 : length(var.resource_ids)
+    for_each = {for idx,rid in var.resource_ids: idx => rid }
 
-    name                       = "diag-${local.resource_names_from_ids[count.index]}"
-    target_resource_id         = var.resource_ids[count.index]
+    name                       = "diag-${local.resource_names_from_ids[each.key]}"
+    target_resource_id         = each.value
     log_analytics_workspace_id = local.law_id
-    
-    dynamic "log" {
-        for_each = data.azurerm_monitor_diagnostic_categories.diag_log_categories[count.index].log_category_types
 
+    dynamic "enabled_log" {
+        for_each = data.azurerm_monitor_diagnostic_categories.diag_log_categories[each.key].log_category_types
         content {
-            category = log.value
+            category = enabled_log.value
+
             retention_policy {
-                days    = var.retention_days
-                enabled = true
+                enabled = false
             }
         }
     }
-
   
 }
