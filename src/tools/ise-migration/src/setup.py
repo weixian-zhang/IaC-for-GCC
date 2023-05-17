40c3d40444e3e7be106ca232bde351ca7f95d31f
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-05-17 14:31:04 +0800

    feat: Add config.yaml for workflow-deployer tool
    - added more workflow folders to test uploading different workflows to upload to specified fileshare
    
    - added setup.py for packaging for release later

---
+++ src/tools/ise-migration/src/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup
+import os 
+
+reqTxtPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(reqTxtPath) as f:
+    required = f.read().splitlines()
+    
+setup(
+    name="deploy-ise-exported-workflows",
+    version="1.0.0",
+    description="Read the latest Real Python tutorials",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=required,  
+    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)
